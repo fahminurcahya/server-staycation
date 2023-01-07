aa281db3c7227f9a53850f757da8b9f3dde52744
Fahmi Nurcahya <fahminurcahya@gmail.com>
2023-01-07 12:16:45 +0700

    dashboard

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -66,11 +66,21 @@ module.exports = {
     res.redirect("/admin/signin");
   },
 
-  viewDashboard: (req, res) => {
-    res.render("admin/dashboard/view_dashboard", {
-      title: "Stycation | Dashboard",
-      user: req.session.user,
-    });
+  viewDashboard: async (req, res) => {
+    try {
+      const member = await Member.find();
+      const booking = await Booking.find();
+      const item = await Item.find();
+      res.render("admin/dashboard/view_dashboard", {
+        title: "Staycation | Dashboard",
+        user: req.session.user,
+        member,
+        booking,
+        item,
+      });
+    } catch (error) {
+      res.redirect("/admin/dashboard");
+    }
   },
   viewCategory: async (req, res) => {
     try {
